7b3ab5d608e10e809469351c9f59c6e17a9cf70a
Bartos Márk <markbartos@protonmail.com>
2021-02-17 17:37:23 +0100

    Cleaned up project. It is ready for use. Please note: Run this code either on physical devices or in watchOS 5 simulator. WatchOS 6 simulator has a bug that does not let transferUserInfo() to run.

--- WCSession WatchKit Extension/InterfaceController.swift
+++ WCSession WatchKit Extension/InterfaceController.swift
@@ -16,29 +16,19 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
     var wcSession : WCSession!
     
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        //print("watchOS - activationDidCompleteWith:", activationState)
     }
     
     func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
-        print("watchOS - didReceiveApplicationContext")
     }
     
-    //Whatever arrives, it will get printed to the console as well as the 'helloLabel' will be changed to help the debugging progress.
-    //BUG: This is the part, that never gets run, even tough the WCSession activated successfully.
     func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : Any] = [:]) {
-        print("watchOS - didReceiveUserInfo")
+        print("watchOS - didReceiveUserInfo", userInfo)
         helloLabel.setText("userInfo")
     }
     
-//    func session(_ session: WCSession, didReceiveUserInfo userInfo: [String : String] = [:]) {
-//        print("watchOS - didReceiveUserInfo")
-//        //helloLabel.setText("didReceiveUserInfo")
-//    }
-//
-    
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        
         print("didReceiveMessage", message)
         let text = message["message"] as! String
         helloLabel.setText(text)
@@ -46,24 +36,18 @@ class InterfaceController: WKInterfaceController, WCSessionDelegate {
     }
     
     func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
-        //replyHandler(["does it work?": "yes sir"])
-        //print("watchOS - didReceiveMessage", message)
-        //helloLabel.setText("didReceiveMessage")
     }
     
     
     //Setting the Interface Controller as WCSession Delegate
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        //session.delegate = self
-        //session.activate()
     }
     
     
     //Activating the session on the watchOS side as well.
     override func willActivate() {
         super.willActivate()
-        
         wcSession = WCSession.default
         wcSession.delegate = self
         wcSession.activate()
--- WCSession/ViewController.swift
+++ WCSession/ViewController.swift
@@ -14,36 +14,14 @@ class ViewController: UIViewController, WCSessionDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //lehet kéne még egy check, hogy if WCSession.avalaible()
-        //az eredeti, variable nélküli így nézett ki:
-        // let session = WCSession.default
-        //        } else if WCSession.isSupported() {
-        //            session.delegate = self
-        //            session.activate()
-        
         wcSession = WCSession.default
         wcSession.delegate = self
         wcSession.activate()
-        
     }
+    
     
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-//        print("WCSession - activationDidCompleteWith:", activationState, "and error code:", error as Any)
-//        switch activationState {
-//            case .activated:
-//                print("WCSession - activationDidCompleteWith .activated")
-//                //session.transferUserInfo(dictionaryToPass)
-//            case .inactive:
-//            print("WCSession - activationDidCompleteWith .inactive")
-//            case .notActivated:
-//            print("WCSession - activationDidCompleteWith .notActivated")
-//            default:
-//                print("WCSession - activationDidCompleteWith: something other ")
-//                break
-//            }
     }
-    
     
     func sessionDidBecomeInactive(_ session: WCSession) {
         print("WCSession - sessionDidBecomeInactive")
@@ -52,52 +30,21 @@ class ViewController: UIViewController, WCSessionDelegate {
         print("WCSession - sessionDidDeactivate")
     }
     
+    
     var dataToSend = ["userInfo":"júzerInfó"]
+    
     
     //Pushing the button on the iOS storyboard will attempt iOS-watchOS connection.
     @IBAction func tuiButton(_ sender: UIButton) {
-        
-        //let dictionaryToPass = ["product1": 0, "product2": 0]
-        //let userInfo = []
-        
-        //try? wcSession.updateApplicationContext(dataToSend)
-        //else {print("Nem sikerült a try")}
-        
-        //var data = dataToSend
         wcSession.transferUserInfo(dataToSend)
-        
-        //wcSession.transferUserInfo(["it works": "yeah kinda","but for real": "does it work?"])
-        //wcSession.transferUserInfo(dictionaryToPass)
-        
-//        let session = WCSession.default
-//        if session.isReachable {
-//            session.transferUserInfo(dictionaryToPass)
-//        } else if WCSession.isSupported() {
-//            session.delegate = self
-//            session.activate()
-//        }
     }
+    
     @IBAction func sendmButton(_ sender: UIButton) {
-        
         let txt = "hellocska"
         let message = ["message":txt]
-        
         wcSession.sendMessage(message, replyHandler: nil) { (error) in
-            
             print(error.localizedDescription)
-            
         }
-        
-        
-//        let session = WCSession.default
-//        if session.isReachable {
-//            session.sendMessage(dictionaryToPass, replyHandler: { reply in
-//                print(reply)
-//            }, errorHandler: nil)
-//        } else if WCSession.isSupported() {
-//            session.delegate = self
-//            session.activate()
-//        }
     }
     
     
